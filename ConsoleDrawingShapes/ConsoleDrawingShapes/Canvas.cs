ed00f30f4c3040dfc9d1e7fc7d945727362eeca3
Damian <damian.flexsoft@gmail.com>
2021-09-11 17:34:13 +0200

    work: canvas base methods declarations modified

--- ConsoleDrawingShapes/ConsoleDrawingShapes/Canvas.cs
+++ ConsoleDrawingShapes/ConsoleDrawingShapes/Canvas.cs
@@ -14,10 +14,14 @@ namespace ConsoleDrawingShapes
                 throw new ArgumentOutOfRangeException();
 
             _height = height + 2;
+
+            _content = new Point[_width, _height];
         }
 
         private readonly int _width;
         private readonly int _height;
+        private readonly Point[, ] _content;
+        private const char DefaultColor = 'x';
 
         /// <summary>
         /// Create canvas with specified with and heigh.
@@ -26,24 +30,24 @@ namespace ConsoleDrawingShapes
         /// <param name="heigh">The maximum width is 300</param>
         public static Canvas Create(int width, int heigh) => new Canvas(width, heigh);
 
-        public void DrawLine(int x1, int y1, int x2, int y2)
-        { 
-        
+        public void DrawLine(int x1, int y1, int x2, int y2, char color = DefaultColor)
+        {
+            var line = new Line(x1, y1, x2, y2, color);
         }
 
-        public void DrawRectangle(int x1, int y1, int x2, int y2)
-        { 
-        
-        }
-
-        public void Fill(int x, int y, char colour)
-        { 
+        public void DrawRectangle(int x1, int y1, int x2, int y2, char color = DefaultColor)
+        {
 
         }
 
-        private void DrawPoint(int x, int y, char colour)
-        { 
-        
+        public void Fill(int x, int y, char color = DefaultColor)
+        {
+
+        }
+
+        private void DrawPoint(int x, int y, char color = DefaultColor)
+        {
+
         }
     }
 }
