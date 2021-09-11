99a7039182d1f00519d3dc5ac27379e7cd3d19c0
Damian <damian.flexsoft@gmail.com>
2021-09-11 16:18:36 +0200

    work: canvas first draft ready

---
+++ ConsoleDrawingShapes/ConsoleDrawingShapes/Canvas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleDrawingShapes
+{
+    public sealed class Canvas
+    {
+        private Canvas(int width, int height) {
+            if (width < 1 || width > 300)
+                throw new ArgumentOutOfRangeException();
+
+            _width = width + 2;
+
+            if (height < 1 || height > 300)
+                throw new ArgumentOutOfRangeException();
+
+            _height = height + 2;
+        }
+
+        private readonly int _width;
+        private readonly int _height;
+
+        /// <summary>
+        /// Create canvas with specified with and heigh.
+        /// </summary>
+        /// <param name="width">The maximum width is 300</param>
+        /// <param name="heigh">The maximum width is 300</param>
+        public static Canvas Create(int width, int heigh) => new Canvas(width, heigh);
+
+        public void DrawLine(int x1, int y1, int x2, int y2)
+        { 
+        
+        }
+
+        public void DrawRectangle(int x1, int y1, int x2, int y2)
+        { 
+        
+        }
+
+        public void Fill(int x, int y, char colour)
+        { 
+
+        }
+
+        private void DrawPoint(int x, int y, char colour)
+        { 
+        
+        }
+    }
+}
---
+++ ConsoleDrawingShapes/UnitTests/CanvasTests.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleDrawingShapes;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class CanvasTests
+    {
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0, 10)]
+        [InlineData(10, 0)]
+        [InlineData(-1, 10)]
+        [InlineData(-1, -1)]
+        [InlineData(-10, -1)]
+        [InlineData(301, 20)]
+        [InlineData(301, 302)]
+        [InlineData(15, 302)]
+        public void Create_Canvas_Unsuccessful(int width, int height)
+        {
+            Action action = () => Canvas.Create(width, height);
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+    }
+}
